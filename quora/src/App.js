ab3025a12ed10f7361e454ca090bd9fc6f2f3ba7
RokoPavelic <rokoprivatno@gmail.com>
2022-05-31 11:58:29 +0200

    react

---
+++ quora/src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { Question } from "./Components/Question";
+
+export function App() {
+  return (
+    <div>
+      <Question
+        title="What’s the big deal with React.js?"
+        text="What are some facts about why React is technically superior"
+      />
+      <Question title="Is React.js worth it?" text="Let us know" />
+      <Question
+        title="Should I learn React.js or Vue.js?"
+        text="Basically is it worth it if I learn Vue.js first and then React.js"
+      />
+    </div>
+  );
+}
---
+++ quora/src/Components/Question.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Answer } from "./Answer";
+
+export function Question(props) {
+  return (
+    <div className="question">
+      <h3 className="question__title">{props.title}</h3>
+      <p className="question__definition">{props.text}</p>
+      <Answer answer="Answer 1" />
+      <Answer answer="Answer 2" />
+    </div>
+  );
+}
